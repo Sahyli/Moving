0033a91337380c118dd32c609ce19b6f65af4496
Sahylí <148484230+Sahyli@users.noreply.github.com>
2023-11-15 12:48:10 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,3 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
     music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
 })
-
